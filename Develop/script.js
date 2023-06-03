44c62ea56a29e7ee4e07cc58b74d8dd93daaaec4
evan-pieper <evanjpieper@gmail.com>
2023-06-02 20:36:41 -0700

    program now capable of writing a password, TODO: implement lower characters , numerical characters, and special characters.

--- Develop/script.js
+++ Develop/script.js
@@ -11,12 +11,60 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var passwordBox = document.getElementById("password");
-  var password = document.createElement("p");
+  var passString = "";
+  var passArray = [];
   var passLength = 0;
-  var characterSet = 0;
+  var characterSet = [];
+  var charIndex = 0;
+  var charIndexCopy = 0;
+  var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+  passLength = prompt("how long would you like your password to be? (input a number between 8 and 128");
+  while(passLength < 8 || passLength > 128)
+  {
+    passLength = prompt("please input a valid number between 8 and 128");
+  }
+
+
+  if(confirm("do you want uppercase letter?")== true){
+    for(i= charIndex; i< charIndexCopy + upperChar.length; i++){
+      characterSet[i] = upperChar[i-charIndexCopy]; 
+
+      charIndex = i;
+    }
+    charIndexCopy = charIndex;
+  }
+
+  /*
+  if(confirm("do you want lowercase letter?")== true){
+    for(i=0; i<lowerChar.length; i++){
+      characterSet[i+charIndex] = lowerChar[i]; 
+    }
+  }
+
+  if(confirm("do you want numerical characters?")== true){
+
+  }
+
+  if(confirm("do you want special characters?")== true){
+
+  }
+*/
+
+
+  for(i=0;i<passLength;i++){
+    passArray[i] = characterSet[Math.floor(Math.random()*passLength)];
+    passString = passString + passArray[i];
+  }
+
+  /*
+  password.innerHTML = passArray;
   passwordBox.appendChild(password);
-  return password;
+  */
+  return passString;
 }
 
 // Add event listener to generate button
